4b74cfd2ec8dae0131924abd0f5e4a5f6a8c1fdf
Shubhesh Srivastava <107806259+HellcodesIn@users.noreply.github.com>
2022-08-13 17:49:58 +0530

    Update SSR.ts

--- SSR/SSR.ts
+++ SSR/SSR.ts
@@ -1,3 +1,4 @@
+
 //expressjs
 const express = require('express');
 const app = express();
@@ -12,7 +13,14 @@ const port = 3000 || process.env.PORT;
 //Confuguring the server
 
 app.get('/', function(req, res) {
-  res.sendFile(path.resolve('/MainPage/index.html'));
+  res.sendFile(path.resolve('./MainPage/index.html'));
+  app.use(express.static('MainPage'));
+  res.sendFile(path.resolve('./OurTeam/teamStats.html'));
+  app.use(express.static('OurTeam'))
+  res.sendFile(path.resolve('.'));
+  app.use(express.static(''))
+  res.sendFile(path.resolve('.'));
+  app.use(express.static(''))
 });
 
 app.listen(port);
