4f2a73b5a750ff368a69ce3fe5bcb6410ed4441a
Shubhesh Srivastava <107806259+HellcodesIn@users.noreply.github.com>
2022-08-13 17:50:59 +0530

    Update SSR.js

--- SSR/SSR.js
+++ SSR/SSR.js
@@ -7,7 +7,8 @@ var path = require('path');
 var port = 3000 || process.env.PORT;
 //Confuguring the server
 app.get('/', function (req, res) {
-    res.sendFile(path.resolve('/MainPage/index.html'));
+    res.sendFile(path.resolve('./MainPage/index.html'));
+    app.use(express.static('MainPage'));
 });
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
